160488f384cf26026418f8058b3d241b7239b02a
Alex Trost <19617280+a-trost@users.noreply.github.com>
2021-11-30 13:55:57 -0500

    Delete unused files

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,12 +2,6 @@ import NextAuth from "next-auth";
 import TwitchProvider from "next-auth/providers/twitch";
 
 export default NextAuth({
-  // Configure one or more authentication providers
-  callbacks: {
-    async signIn(props) {
-      return true;
-    },
-  },
   providers: [
     TwitchProvider({
       clientId: process.env.TWITCH_CLIENT_ID,
--- pages/api/data.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-  console.log("🚀 ~ file: data.js ~ line 2 ~ handler ~ req", req);
-
-  res.status(200).json({ name: "John Doe" });
-}
